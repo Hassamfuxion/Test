7d665934f3358f3ae16a6818fb618ec505b1a7ed
Hassam990 <133476966+Hassam990@users.noreply.github.com>
2025-04-18 21:12:55 +0500

    Update service.js

--- my-app/src/service.js
+++ my-app/src/service.js
@@ -52,7 +52,7 @@ const SSConsultant1 = () => {
      <Helmet>
         <meta charset="UTF-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <title>SSCONSULTANT</title>
+        <title>S.S. Consultant - Software Solution & Consultancy</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Favicon */}
