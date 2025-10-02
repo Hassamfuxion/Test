34d7b50d34ae27ffad84ab498f830774a4286400
Hassam990 <hassamjahanzaib@fuxionsoft.com>
2025-10-02 17:52:16 +0500

    Update spline.js

--- my-app/src/spline.js
+++ my-app/src/spline.js
@@ -10,7 +10,7 @@ export default function HeroVideo() {
       }}
     >
       <iframe
-        src="https://player.vimeo.com/video/1122926131?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&muted=1"
+        src="https://player.vimeo.com/video/1123874672?badge=0&autopause=0&player_id=0&app_id=58479"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
@@ -22,7 +22,7 @@ export default function HeroVideo() {
           height: "100%",
           borderRadius: "16px", // rounded corners
         }}
-        title="SSC Video Final for website"
+        title="final yt audio fixed"
       ></iframe>
     </div>
   );
